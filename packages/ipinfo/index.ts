fb777a572b3a0f0075b62fc7f968d047bfe2cca2
tpguy825 <75158953+tpguy825@users.noreply.github.com>
2025-08-16 21:49:42 +0100

    Init + added ipinfo

---
+++ packages/ipinfo/index.ts
@@ -0,0 +1,40 @@
+import { type CountryResponse, Reader } from "mmdb-lib";
+
+// GeoIP data from https://github.com/sapics/ip-location-db/tree/main/geolite2-country-mmdb
+const rawipdb = await fetch(
+	"https://raw.githubusercontent.com/sapics/ip-location-db/refs/heads/main/geolite2-country-mmdb/geolite2-country.mmdb",
+)
+	.then((r) => r.arrayBuffer())
+	.then((r) => Buffer.from(r));
+
+const ipdb = new Reader<CountryResponse>(rawipdb);
+
+const port = 48292; // change in Dockerfile too
+
+Bun.serve({
+	port,
+	fetch(request, server) {
+		try {
+			const { pathname, origin } = new URL(request.url);
+			if (pathname === "/")
+				return json(
+					400,
+					`{"error":"No IP address provided, please try again with an IP. (e.g. ${origin}/1.1.1.1). You can find your IP address by visiting https://icanhazip.com"}`,
+				);
+			const res = ipdb.get(pathname.slice(1));
+			return res ? json(200, res) : json(404, '{"error":"IP not found"}');
+		} catch (e) {
+			console.error(e);
+			return json(500, '{"error":"Internal server error"}');
+		}
+	},
+});
+
+console.log("Listening on http://127.0.0.1:" + port);
+
+function json(status: number, text: string | object) {
+	return new Response(typeof text === "string" ? text : JSON.stringify(text), {
+		status,
+		headers: { "Content-Type": "application/json" },
+	});
+}
